3068591a8888590c205243b1f92b585130dec9c4
1090482421041 <andresa.paula1981@gmail.com>
2026-03-06 09:12:41 -0300

    (06/03) Criação da primeira classe (Pessoa)

---
+++ empresa2/src/Pessoa.java
@@ -0,0 +1,35 @@
+public class Pessoa {
+
+    // Atributos
+    public String nome;
+    public Integer idade;
+    public String cpf;
+    public String email;
+    public String telefone;
+
+    // Métodos
+    public void apresentar() {
+        System.out.println("Olá! Meu nome é " + this.nome + " e tenho " + this.nome + "anos.");
+    }
+
+    public void fazerAniversario() {
+        this.idade++; // Incremento a idade
+        System.out.println("Feliz aniversário, " + this.nome + "! Agora você tem " + this.nome + "anos.");
+    }
+
+    public void atualizarEmail(String novoEmail) {
+        this.email = novoEmail;
+        System.out.println("Email atualizado para: " + this.email);
+    }
+
+    public String exibirDados() {
+        // \n significa quebra de linha
+        return  "Nome:" + this.nome +
+                "\nIdade:" + this.idade +
+                "\nCPF:" + this.cpf +
+                "\nTelefone:" + this.telefone;
+
+    }
+
+}
+
