a6e5cbd2ef4b15a4f65f6f64f08f35126f09e5d0
Heitor-Tasso <heitortasso12@gmail.com>
2026-03-10 11:18:08 -0300

    (10/03) Definição da classe Funcionário para herança de Pessoa

--- empresa/src/Main.java
+++ empresa/src/Main.java
@@ -2,14 +2,44 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Programa iniciado!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Criação do primeiro objeto de pessoa
+        Pessoa pessoa1 = new Pessoa();
+
+        // Atribuindo valores aos atributos
+        pessoa1.nome = "Gervásio Gomes Garcia";
+        pessoa1.idade = 54;
+        pessoa1.cpf = "123.456.789-01";
+        pessoa1.email = "gervasio.garcia@empresa.com.br";
+        pessoa1.telefone = "(16) 98765-4321";
+
+        // Exibindo os dados da pessoa 1
+        System.out.println(pessoa1.mostrarDados());
+
+        System.out.println("-".repeat(80));
+
+        pessoa1.fazerAniversario();
+
+        // Apresentação da pessoa após aniversário
+        pessoa1.apresentar();
+
+        System.out.println("-".repeat(80));
+
+        // Atualizando o e-mail
+        pessoa1.atualizarEmail("ggarcia@outlook.com");
+
+        System.out.println("-".repeat(80));
+
+        // Exibindo dados atualizados
+        System.out.println(pessoa1.mostrarDados());
+
+        // Alterando novamente o e-mail por manipulação direta
+        pessoa1.email = "garcia.gervasio@bol.com.br";
+
+        System.out.println("-".repeat(80));
+
+        // Exibindo dados finais
+        System.out.println(pessoa1.mostrarDados());
+
     }
 }
