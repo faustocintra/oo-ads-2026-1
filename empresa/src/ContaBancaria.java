46df6c37da2fe199a004dbc70b28a60cde466a2a
Samuel-dev-97 <samuelpssouza@gmail.com>
2026-03-22 17:41:46 +0000

    17/03) Introdução de construtores personalizados nas classes Pessoa e Funcionario

--- empresa/src/ContaBancaria.java
+++ empresa/src/ContaBancaria.java
@@ -1,5 +1,32 @@
 public class ContaBancaria {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+    // Atributos
+    public Double saldo;
+    public String titular;
+
+    // Construtor para facilitar a criação na Main
+    public ContaBancaria(String titular, Double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    // Métodos solicitados
+    public void depositar(Double quantia) {
+        if (quantia > 0) {
+            this.saldo += quantia;
+            System.out.println("Depósito de R$ " + quantia + " realizado.");
+        }
+    }
+
+    public void sacar(Double quantia) {
+        if (quantia > 0 && quantia <= this.saldo) {
+            this.saldo -= quantia;
+            System.out.println("Saque de R$ " + quantia + " realizado.");
+        } else {
+            System.out.println("Saldo insuficiente ou quantia inválida.");
+        }
+    }
+
+    public void exibirSaldo() {
+        System.out.println("Titular: " + this.titular + " | Saldo atual: R$ " + this.saldo);
     }
 }
--- empresa/src/Main.java
+++ empresa/src/Main.java
@@ -1,15 +1,14 @@
 import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Agora o main chama os três testes em sequência
         teste01();
         teste02();
+        teste03();
     }
 
     public static void teste01() {
-        // Criação do primeiro objeto de pessoa
         Pessoa pessoa1 = new Pessoa(
                 "Gervásio Gomes Garcia",
                 54,
@@ -18,45 +17,12 @@ public class Main {
                 "(16) 98765-4321"
         );
 
-        // Atribuindo valores aos atributos
-//        pessoa1.nome = "Gervásio Gomes Garcia";
-//        pessoa1.idade = 54;
-//        pessoa1.cpf = "123.456.789-01";
-//        pessoa1.email = "gervasio.garcia@empresa.com.br";
-//        pessoa1.telefone = "(16) 98765-4321";
-
-        // Chamando o método para exibir os dados da pessoa 1
         System.out.println(pessoa1.exibirDados());
-
-        System.out.println("-".repeat(80));  // Traço separador
-
+        System.out.println("-".repeat(80));
         pessoa1.fazerAniversario();
-
-        // Chamando a apresentação da pessoa, que reflete a nova idade
         pessoa1.apresentar();
+        System.out.println("-".repeat(80));
 
-        System.out.println("-".repeat(80));  // Traço separador
-
-        // Atualizando o e-mail
-        pessoa1.atualizarEmail("ggarcia@outlook.com");
-
-        System.out.println("-".repeat(80));  // Traço separador
-
-        // Exibindo novamente os dados para conferir as alterações feitas
-        System.out.println(pessoa1.exibirDados());
-
-        // Alterando novamente o e-mail por meio de manipulação direta
-        // do valor do atributo
-        pessoa1.email = "garcia.gervasio@bol.com.br";
-
-        System.out.println("-".repeat(80));  // Traço separador
-
-        // Exibindo novamente os dados para conferir as alterações feitas
-        System.out.println(pessoa1.exibirDados());
-
-        System.out.println("*".repeat(80));
-
-        // Vamos criar o primeiro objeto da classe Funcionario
         Funcionario func1 = new Funcionario(
                 "Veneranda Vieira",
                 26,
@@ -66,51 +32,33 @@ public class Main {
                 "F0123",
                 "Assistente Financeiro",
                 3500.00,
-                "Gerência Financeira"   
+                "Gerência Financeira"
         );
 
-
-        // Dando valor aos atributos do funcionário
-        // (inclui atributos herdados de Pessoa)
-        //func1.nome = "Veneranda Vieira";
-        //func1.idade = 26;
-        //func1.cpf = "987.654.321-09";
-        //func1.email = "veneranda@empresa.com.br";
-        //func1.telefone = "(36) 91234-5678";
-        //----//
-        //func1.matricula = "F0123";
-        //func1.cargo = "Assistente Financeiro";
-        //func1.salarioBase = 3500.00;
-        //func1.departamento = "Gerência Financeira";
-       //func1.dataAdmissao = new Date();
-
-        // Exibindo os dados do funcionário
         System.out.println(func1.exibirDados());
-
         System.out.println("-".repeat(80));
 
-        func1.fazerAniversario();
-
-        // Exibindo os dados novamente para verificar mudança de idade
-        System.out.println(func1.exibirDados());
-
-        System.out.println("-".repeat(80));
-
-        // Calculando o salário de Veneranda
         Double salarioFinal = func1.calcularSalario();
         System.out.println("O salário final de Veneranda é R$ " + salarioFinal);
     }
 
     public static void teste02() {
+        // Método mantido como no seu original (vazio ou comentado)
+        System.out.println("\nExecutando Teste 02...\n");
+    }
 
-//        System.out.println("%".repeat(80));
-//
-//        Pessoa p = new Pessoa();
-//        System.out.println("Exibindo dados de uma pessoa...");
-//        System.out.println(p.exibirDados());
-//
-//        Funcionario f = new Funcionario();
-//        System.out.println("Exibindo dados de um funcionário...");
-//        System.out.println(f.exibirDados());
+    // NOVO MÉTODO: Aqui é onde você usa a classe ContaBancaria
+    public static void teste03() {
+        System.out.println("\n" + "=".repeat(20) + " EXERCÍCIO CONTA BANCÁRIA " + "=".repeat(20));
+
+        // Criando o objeto da conta
+        ContaBancaria conta = new ContaBancaria("AJAX", 1000.0);
+
+        // Executando os comandos solicitados no exercício
+        conta.exibirSaldo();
+        conta.depositar(200.0);
+        conta.exibirSaldo();
+        conta.sacar(500.0);
+        conta.exibirSaldo();
     }
 }
