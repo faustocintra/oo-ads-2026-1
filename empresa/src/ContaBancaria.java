8af99a6ed63b780da68c956c721c22f1c5d9cd2d
Renata Claudino <118945461+renataclaudino@users.noreply.github.com>
2026-03-30 14:16:29 -0300

    trabalho 1 POO

---
+++ empresa/src/ContaBancaria.java
@@ -0,0 +1,31 @@
+public class ContaBancaria {
+    // Atributos
+    public Double saldo;
+    public String titular;
+
+    // CONSTRUTOR PERSONALIZADO
+    // Exige titular e saldo inicial para o objeto passar a existir
+    public ContaBancaria(String titular, Double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(Double quantia) {
+        this.saldo += quantia;
+        System.out.println("Depósito de R$ " + quantia + " para " + this.titular);
+    }
+
+    public void sacar(Double quantia) {
+        if (quantia <= this.saldo) {
+            this.saldo -= quantia;
+            System.out.println("Saque de R$ " + quantia + " realizado por " + this.titular);
+        } else {
+            System.out.println("Saldo insuficiente para " + this.titular);
+        }
+    }
+
+    public void exibirSaldo() {
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo: R$ " + this.saldo);
+    }
+}
